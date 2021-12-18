4907a404550e2c4dc2d29e4fb491457462a89eb1
PgmJun <84304802+PgmJun@users.noreply.github.com>
2021-12-18 23:44:33 +0900

    주차장 데이터 CSV 파일 24시간 단위로 자동 최신화 기능 추가, CSV 파일 파싱 역할 분할
    ** 기능 구현
    - AutoCsvParser.java : @Scheduled 어노테이션과 Timer 클래스, TimeTask 클래스를 이용한 스케쥴링 작업으로 CSV 파일 최신화 배치파일을 매일 0시마다 자동으로 실행하도록 구현.
    ** 수정
    - ParkingServiceApplication.java :
    1. 메인메서드에서 직접 수행되던 CSV 파싱 자동화 기능을 AutoCsvParser 클래스로 분리,
    2. 자동 작업 수행을 위해 @EnableScheduling 어노테이션 추가
    ** 오류 해결
    -
    ** 미해결 오류

---
+++ src/main/java/jh/ParkingService/AutoCsvParser.java
@@ -0,0 +1,46 @@
+package jh.ParkingService;
+
+import jh.ParkingService.repository.park.ParkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Component
+public class AutoCsvParser {
+
+    private ParkRepository parkingRepository;
+
+    @Autowired
+    public AutoCsvParser(ParkRepository parkingRepository) {
+        this.parkingRepository = parkingRepository;
+    }
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void parse(){
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                Runtime runtime = Runtime.getRuntime();
+                try {
+                    Process p1 = runtime.exec("cmd /c start D:\\project\\ParkingService\\src\\main\\java\\jh\\ParkingService\\repository\\park\\autoCsvInstall.bat");
+                    InputStream is = p1.getInputStream();
+                    int i = 0;
+                    while( (i = is.read() ) != -1) {
+                        System.out.print((char)i);
+                    }
+
+                } catch(IOException ioException) {
+                    System.out.println(ioException.getMessage() );
+                }
+            }
+        }; timer.schedule(task, 1);
+    }
+
+}
--- src/main/java/jh/ParkingService/ParkingServiceApplication.java
+++ src/main/java/jh/ParkingService/ParkingServiceApplication.java
@@ -1,9 +1,15 @@
 package jh.ParkingService;
 
 import jh.ParkingService.repository.park.JpaParkRepository;
+import jh.ParkingService.repository.park.ParkRepository;
+import jh.ParkingService.service.park.ParkServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.persistence.EntityManager;
 import java.io.IOException;
@@ -11,6 +17,7 @@ import java.io.InputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
+@EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
 class ParkingServiceApplication {
@@ -18,25 +25,6 @@ class ParkingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingServiceApplication.class, args);
 
-		Timer timer = new Timer();
-		TimerTask task = new TimerTask() {
-			@Override
-			public void run() {
-				Runtime runtime = Runtime.getRuntime();
-				try {
-					Process p1 = runtime.exec("cmd /c start D:\\project\\ParkingService\\src\\main\\java\\jh\\ParkingService\\repository\\park\\autoCsvInstall.bat");
-					InputStream is = p1.getInputStream();
-					int i = 0;
-					while( (i = is.read() ) != -1) {
-						System.out.print((char)i);
-					}
-
-				} catch(IOException ioException) {
-					System.out.println(ioException.getMessage() );
-				}
-			}
-		};
-		timer.scheduleAtFixedRate(task, 1000000, 86400000); // 1초 뒤 하루마다 반복실행
 	}
 
 }

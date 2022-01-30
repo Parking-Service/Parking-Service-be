fb93b68cf363eb0423fc299980b69eded9cd9347
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-01-30 13:37:53 +0900

    Update ParkServiceImpl.java

--- src/main/java/jh/ParkingService/service/park/ParkServiceImpl.java
+++ src/main/java/jh/ParkingService/service/park/ParkServiceImpl.java
@@ -1,49 +1,33 @@
 package jh.ParkingService.service.park;
 
-import jh.ParkingService.csv.CSVParser;
-import jh.ParkingService.domain.park.Park;
-import jh.ParkingService.domain.park.ParkRepository;
+import jh.ParkingService.domain.Park;
+import jh.ParkingService.repository.park.ParkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.List;
 
-@RequiredArgsConstructor
-@Primary
-@Transactional
-@Component
-public class ParkServiceImpl {
-    private final EntityManager em;
 
-    @Autowired
-    private ParkRepository parkRepository;
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ParkServiceImpl {
+    private final ParkRepository parkingRepository;
+
 
     public void saveData(){
-            //PARK_DATA 초기화
-            em.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
-            em.createQuery("DELETE FROM Park").executeUpdate();
-            em.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
-
-            CSVParser parser = new CSVParser(em);
-            parser.read();
-
+        parkingRepository.save();
     }
 
-    public List<Park> searchAddr(String addr, String lat, String lng){ return parkRepository.findByAddr(addr, lat, lng); }
+    public List<Park> searchAddr(String addr, String lat, String lng){ return parkingRepository.findByAddr(addr, lat, lng); }
 
-    public List<Park> searchLotLoc(String lat, String lng){
-        Double dLat = Double.parseDouble(lat);
-        Double dLng = Double.parseDouble(lng);
-        Double n = 0.02;
+    public List<Park> searchLotLoc(String lat, String lng){ return parkingRepository.findByLocation(lat, lng); }
 
-        return parkRepository.findByLocation(dLat, dLng, n);
-    }
+    public List<Park> searchTel(String telnum) { return parkingRepository.findByTel(telnum); }
 
-    public List<Park> searchTel(String telnum) { return parkRepository.findByPhoneNumberLike(telnum); }
-
-    public List<Park> searchAll() { return parkRepository.findAll(); }
+    public List<Park> searchAll() { return parkingRepository.findAll(); }
 }

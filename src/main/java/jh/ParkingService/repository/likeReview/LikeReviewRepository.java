32c84389b05e3c25a05c090a8b5ba5cbea6543a4
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-02-06 23:52:23 +0900

    Delete src/main/java/jh/ParkingService/repository directory

--- src/main/java/jh/ParkingService/repository/likeReview/LikeReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package jh.ParkingService.repository.likeReview;
-
-public interface LikeReviewRepository {
-    void firstLikeCheck(int likeReviewUid, String likeUserUid);
-    void like(int likeReviewUid, String likeUserUid, boolean like);
-    void removeLike(int reviewUid);
-
-}
--- src/main/java/jh/ParkingService/repository/park/autoCsvInstall.bat
+++
@@ -1,7 +0,0 @@
-D:
-
-cd \project\ParkingService\src\main\java\jh\ParkingService\repository\park
-
-curl https://www.data.go.kr/download/15012896/standard.do?dataType=csv > data.csv
-
-exit
